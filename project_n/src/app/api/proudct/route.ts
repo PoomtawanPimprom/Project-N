d75f566224010b323fbd7c75bdb7880c41abfc3d
Poomtawan Pimprom <poomtawan.work@gmail.com>
2024-09-03 14:10:26 +0700

    create API, Interface, Layout folder

---
+++ project_n/src/app/api/proudct/route.ts
@@ -0,0 +1,13 @@
+export async function GET() {
+    
+}
+
+export async function POST() {
+    
+}
+export async function PUT() {
+    
+}
+export async function DELTE() {
+    
+}
--- project_n/src/app/layout.tsx
+++ project_n/src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Navbar from "./component/navbar";
+import Navbar from "./layout/navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
