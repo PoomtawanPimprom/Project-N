dc3d46f6ce9a0cf5fa365b1ecbeaf0224c12e418
Poomtawan Pimprom <poomtawan.work@gmail.com>
2024-09-24 23:45:47 +0700

    create product service

--- project_n/src/app/api/product/[id]/route.ts
+++ project_n/src/app/api/product/[id]/route.ts
@@ -2,6 +2,7 @@ import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
 
+
 export async function GET(request: Request, { params }: { params: { id: string } }) {
     const productID = Number(params.id)
     try {
---
+++ project_n/src/app/service/product/service.ts
@@ -0,0 +1,57 @@
+//POST /api/product
+export async function CreateProdcut(data:any) {
+    const res = await fetch('/api/product',{
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body : JSON.stringify(data)
+    });
+
+    if(!res.ok){
+        return res.json();
+    }
+}
+
+//GET /api/product
+export async function getAllproduct() {
+    const res = await fetch(`/api/product`, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json"
+        }
+    });
+    return res.json();
+}
+
+//GET /api/product/:id
+export async function getProductByProductId(id:number) {
+    const res = await fetch(`/api/product/${id}`, {
+        method: "GET",
+        headers:{
+            "Content-Type": "application/json",
+        },
+    });
+    return res.json();
+}
+
+//PUT /api/product/:id
+export async function updateProductbyID(id:number,data:any) {
+    const res = await fetch(`/api/product/${id}`, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+    });
+}
+
+//DELETE /api/product/:id
+export async function deleteProductByID(id:number) {
+    const res = await fetch(`/api/product/${id}`, {
+        method: "DELETE",
+        headers: {
+            "Content-Type": "application/json",
+        },
+    });
+}
