74446fea7f74d063107ba4f13b522cbe2b47f6bd
Poomtawan Pimprom <poomtawan.work@gmail.com>
2024-09-29 15:17:35 +0700

    create user table, init value role, gender, userStatus

--- project_n/prisma/seed.ts
+++ project_n/prisma/seed.ts
@@ -17,10 +17,31 @@ const categoryData = [
     { id: 2, name: "sport", },
 ];
 
+const roleData = [
+    { id: 1, name: "user", },
+    { id: 2, name: "admin", },
+    { id: 3, name: "super admin", },
+]
+
+const userStatusData = [
+    { id: 1, name: "available" },
+    { id: 2, name: "unavailable" },
+]
+
+const gednerData =[
+    { id: 1, name: "Male", },
+    { id: 2, name: "Female", },
+    { id: 3, name: "LGBTQIA+", }
+]
+
+
 async function main() {
     await prisma.productStatus.createMany({ data: productStatusData })
     await prisma.storeStatus.createMany({ data: storeStatusData })
     await prisma.category.createMany({ data: categoryData })
+    await prisma.gender.createMany({data:gednerData})
+    await prisma.userStatus.createMany({data:userStatusData})
+    await prisma.role.createMany({data:roleData})
 }
 
 main()
