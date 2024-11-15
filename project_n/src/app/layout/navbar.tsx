18a4c5a0edc0b6899d0d450cd21ef02a880745b3
NamChoco <numchoco2512@gmail.com>
2024-11-15 11:33:03 +0700

    change ui navbar

--- project_n/src/app/layout/navbar.tsx
+++ project_n/src/app/layout/navbar.tsx
@@ -1,81 +1,48 @@
-"use client";
-import Link from "next/link";
-//icon
-import { MdOutlineShoppingBag } from "react-icons/md";
-import { MdDarkMode } from "react-icons/md";
-import { FaRegCircle } from "react-icons/fa6";
-import { IoSearch } from "react-icons/io5";
-import { RxAvatar } from "react-icons/rx";
+"use client"
+import React, { useState } from 'react'
+import Link from 'next/link'
+import { MdDarkMode, MdOutlineShoppingBag } from 'react-icons/md'
+import { RxAvatar } from 'react-icons/rx'
+import { FiSearch } from 'react-icons/fi'
+import { IoMdMore } from 'react-icons/io'
 
-import { useState } from "react";
-const Navbar = () => {
-  const [darkMode, setDarkMode] = useState<boolean>(false);
-  const handleOnClick = () => {
-    setDarkMode(!darkMode);
-    document.body.classList.toggle("dark");
-  };
+function navbar() {
+
+  const [isMenuMore, setIsMenuMore] = useState(false);
   return (
-    <>
-      <div className="flex w-full h-16 justify-between border-2 border-black dark:bg-black ">
-        <div className="flex w-1/3 items-center m-4 ">
-          <Link
-            href="/"
-            className="flex flex-row w-1/4 h-10 p-2 border-2 border-black rounded-3xl justify-center items-center space-x-2 dark:border-white "
-          >
-            <div className="text-base dark:text-white">
-              <FaRegCircle />
-            </div>
-            <div className="flex text-green font-extrabold dark:text-white">
-              MATTER
-            </div>
-          </Link>
-        </div>
-        <div className="flex w-1/3  items-center m-4 justify-center">
-          <div className="flex  h-10 border-2 border-black rounded-3xl p-1 justify-center dark:border-white">
-            <div className="flex items-center justify-center ">
-              <input
-                type="text"
-                className="h-8 rounded-3xl  p-2 focus:outline-none dark:bg-black dark:text-white"
-                placeholder="Search..."
-              />
-            </div>
-            <div className="flex items-center justify-center ">
-              <button className="flex w-8 h-8 bg-green justify-center items-center rounded-full border-2 border-black p-1 dark:border-white dark:text-white dark:bg-black">
-                <IoSearch />
-              </button>
-            </div>
-          </div>
-        </div>
-        <div className="flex w-1/3 items-center m-4 justify-end">
-          <div className="flex">
-            <div>
-              <div className="flex w-10 h-10 rounded-full border-2 border-black justify-center items-center mx-1 bg-green dark:bg-white">
-                <button onClick={handleOnClick}>
-                  <MdDarkMode />
-                </button>
-              </div>
-            </div>
-            <div>
-              <Link
-                href="/cart"
-                className="flex w-10 h-10 rounded-full border-2 border-black justify-center items-center mx-1 bg-green dark:bg-white"
-              >
-                <MdOutlineShoppingBag className="h-6 w-6 rounded-full " />
-              </Link>
-            </div>
-            <div>
-              <Link
-                href="/profile"
-                className="flex w-10 h-10 bg-green  rounded-full border-2 border-black justify-center items-center mx-1 dark:bg-white"
-              >
-                <RxAvatar className="h-8 w-8 rounded-full" />
-              </Link>
-            </div>
-          </div>
-        </div>
-      </div>
-    </>
-  );
-};
+    <div className='flex justify-between items-center text-black py-4 px-8 md:px-32 bg-white drop-shadow-md'>
+      <Link href='/'>
+        <p className='text-lg font-semibold hover:scale-105 transition-all'>MATTER</p>
+      </Link>
 
-export default Navbar;
+      <div className='relative hidden lg:flex items-center justify-center gap-3'>
+        <FiSearch className='w-5 h-5 absolute left-3'/>
+        <input type="text" placeholder='Search...' className='py-2 pl-10 pr-6 rounded-full border-2'/>
+      </div>
+
+      <ul className='hidden md:flex items-center gap-10 text-base'>
+        <li className='text-lg  hover:text-gray-500 hover:cursor-pointer'>
+          <Link href=''><MdDarkMode className='w-7 h-7' /></Link>
+        </li>
+        <li className='text-lg  hover:text-gray-500 hover:cursor-pointer'>
+          <Link href=''><MdOutlineShoppingBag className='w-7 h-7' /></Link>
+        </li>
+        <li className='text-lg  hover:text-gray-500 hover:cursor-pointer'>
+          <Link href=''><RxAvatar className='w-7 h-7' /></Link>
+        </li>
+      </ul>
+
+      <IoMdMore className='w-7 h-7 md:hidden block cursor-pointer' onClick={() => setIsMenuMore(!isMenuMore)} />
+
+      <div className={`absolute md:hidden top-24 left-0 w-full bg-white flex flex-col items-center gap-6 
+        font-semibold text-lg transform transition-transform ${isMenuMore ? "opacity-100" : "opacity-0"}`} 
+        style={{transition: "transform 0.3s ease, opacity 0.3s ease"}}>
+        <li className='list-none w-full text-center p-4 transition-all cursor-pointer'>Home</li>
+        <li className='list-none w-full text-center p-4 transition-all cursor-pointer'>Cart</li>
+        <li className='list-none w-full text-center p-4 transition-all cursor-pointer'>Account</li>
+      </div>
+    </div>
+  )
+}
+
+export default navbar
