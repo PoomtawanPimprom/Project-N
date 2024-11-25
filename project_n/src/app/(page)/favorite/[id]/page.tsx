4f4245b93f83a6b960a5263b314376dc1e970f9d
Poomtawan Pimprom <poomtawan.work@gmail.com>
2024-11-26 01:06:23 +0700

    update favorite page client to server component

--- project_n/src/app/(page)/favorite/[id]/page.tsx
+++ project_n/src/app/(page)/favorite/[id]/page.tsx
@@ -1,55 +1,66 @@
-"use client";
-
-import { favoriteInterface } from "@/app/interface/favoriteInterface";
-import { deleteFavoriteByid, getAllFavoriteByUserID } from "@/app/service/favorite/service";
+import prisma from "@/lib/prisma/db";
+import { revalidatePath } from "next/cache";
+import Image from "next/image";
+import Link from "next/link";
 import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
 
-const favoriteByUserIdPage = async ({params}: { params: Promise<{ id: number }> }) => {
-  const router = useRouter();
-  const userId = (await params).id;
-  const [favoriteData, setFavoriteData] = useState<favoriteInterface[]>([]);
+const favoriteByUserIdPage = async ({ params }: { params: { id: number } }) => {
+  const userId = Number(params.id);
+  const favoriteData = await prisma.favorite.findMany({
+    where: { userId: userId },
+    include: {
+      product: {
+        select: {
+          name: true,
+          image: true,
+          price: true,
+          store: {
+            select: {
+              name: true,
+            },
+          },
+        },
+      },
+    },
+  });
 
-  const fetchData = async () => {
-    const data = await getAllFavoriteByUserID(userId);
-    setFavoriteData(data);
+  const handleDelete = async (data: FormData) => {
+    "use server";
+    const deleteButton = data.get("deleteButton");
+    await prisma.favorite.delete({
+      where:{id: Number(deleteButton) },
+    })
+    revalidatePath(`/favorite/${userId}`)
   };
-
-  const handleDeleteButton = async (favoriteId:number) => {
-    await deleteFavoriteByid(favoriteId);
-    await fetchData();
-  }
-
-  useEffect(() => {
-    fetchData();
-  }, []);
 
   return (
     <>
-      <div className="flex w-full h-lvh justify-center">
-        <div className="flex flex-col  w-3/5">
+      <div className="flex w-full justify-center">
+        <div className="flex flex-col">
           <div className="header flex flex-col my-2 text-4xl font-bold">
-            <p>Favorite</p>
+            <p>รายการที่ฉันชอบ</p>
           </div>
           <div className="flex flex-col h-full rounded-xl border border-black my-2 p-4">
             {favoriteData.map((item, index) => (
-              <div 
-              onClick={()=>router.push(`/product/${item.productId}`)}
-              className="flex flex-col border border-black rounded-xl p-2">
+              <div
+                key={index}
+                // href={`/product/${item.productId}`}
+                className="flex flex-col border border-black rounded-xl p-2"
+              >
                 <div className="header flex text-2xl font-bold mb-1">
                   <p>{item.product?.store?.name}</p>
                 </div>
                 <div className="body flex justify-between ">
-                  <div>{item.product?.image}</div>
+                  {/* <div><Image width={120} height={120} src={item.im}/></div> */}
                   <div>{item.product?.name}</div>
                   <div>quatity</div>
                   <div>{item.product?.price}</div>
                   <div>
-                    <button
-                      onClick={async ()=>{
-                        await handleDeleteButton(item.id);
-                      } }
-                    >ลบ</button>
+                    <form action={handleDelete}>
+                      <button name="deleteButton" value={item.id}>
+                        ลบ
+                      </button>
+                    </form>
                   </div>
                 </div>
               </div>
