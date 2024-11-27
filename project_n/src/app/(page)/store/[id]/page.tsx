f9938593e28a5a09bd66de22b503b38e866816a2
Poomtawan Pimprom <poomtawan.work@gmail.com>
2024-11-27 11:09:07 +0700

    update store main page to server component
    - rename componentDiscount to Discount

--- project_n/src/app/(page)/store/[id]/page.tsx
+++ project_n/src/app/(page)/store/[id]/page.tsx
@@ -1,18 +1,12 @@
-import { storeInterface } from "@/app/interface/storeInterface";
-
 // components
-import Discout from "../component/ComponentDiscout";
-import ImageStore from "../component/ComponentImageStore";
-import InfoStore from "../component/ComponentInfoStore";
-import ShowProduct from "../component/ComponentShowProduct";
+import Discout from "../component/Discout";
+import ImageStore from "../component/ImageStore";
+import InfoStore from "../component/InfoStore";
+import ShowProduct from "../component/ShowProduct";
 import prisma from "@/lib/prisma/db";
 
-export default async function StorePage(
-  {params}: {
-    params: Promise<{ id: number }>;
-  }
-) {
-  const storeId = Number((await params).id);
+export default async function StorePage({params}: {params: { id: number };}) {
+  const storeId = Number(params.id);
   const store = await prisma.store.findUnique({
     where: { id: storeId },
     include: { user: true, Product: true },
