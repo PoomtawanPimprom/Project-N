240ec057c5ad74c5721d4278042626c7edf27f58
NamChoco <numchoco2512@gmail.com>
2024-12-13 15:06:29 +0700

    add api profile

---
+++ project_n/src/app/api/profile/route.ts
@@ -0,0 +1,90 @@
+import { Prisma, PrismaClient } from '@prisma/client'
+import { NextRequest, NextResponse } from 'next/server';
+import { FaAws } from 'react-icons/fa';
+
+const prisma = new PrismaClient()
+
+export async function GET() {
+    const posts = await prisma.user.findMany()
+    return Response.json(posts)
+}
+
+export async function POST(request: NextRequest) {
+    try {
+        const {
+            name,
+            username,
+            password,
+            email,
+            mobile,
+            birthdate,
+            profile,
+            saler,
+            genderId,
+            roleId,
+            userStatusId,
+            userAddressId
+        } = await request.json();
+
+        await prisma.user.create({
+            data: {
+                name,
+                username,
+                password,
+                email,
+                mobile,
+                birthdate: new Date(birthdate),
+                profile,
+                saler,
+                genderId,
+                roleId,
+                userStatusId,
+                userAddressId
+            }
+        });
+
+        return new NextResponse("User created successfully", { status: 201 });
+    } catch (e: any) {
+        console.error(e);
+        if (e instanceof Prisma.PrismaClientKnownRequestError) {
+            switch (e.code) {
+                case 'P2002':
+                    return new NextResponse("Duplicate field value violates unique constraint", { status: 404});
+                default:
+                    return new NextResponse(`Database error: ${e.message}`, { status: 500});
+            }
+        } else if (e instanceof Prisma.PrismaClientUnknownRequestError) {
+            return new NextResponse("Unknown database error occurred", { status: 500 });
+        } else if (e instanceof Prisma.PrismaClientValidationError) {
+            return new NextResponse(`Validation error: ${e.message}`, { status: 422 });
+        } else if (e instanceof SyntaxError) {
+            return new NextResponse("Invalid JSON payload", { status: 400 });
+        } else if (e instanceof TypeError) {
+            return new NextResponse("Missing or invalid fields", { status: 422 });
+        } else {
+            return new NextResponse("Internal server error", { status: 500 });
+        }
+    }
+}
+
+
+export async function PUT(request: NextRequest, { param } : { param: { id: string}}){
+    try {
+        const { 
+            name,
+            username,
+            password,
+            email,
+            mobile,
+            birthdate,
+            profile,
+            saler,
+            genderId,
+            roleId,
+            userStatusId,
+            userAddressId
+        } = await request.json();
+    } catch (e) {
+
+    }
+}
