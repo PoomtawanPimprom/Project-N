a1ecc590ff0610ab4a39182c81d29a956bb4d0cb
Poomtawan Pimprom <poomtawan.work@gmail.com>
2024-12-14 22:52:27 +0700

    Update next.config.mjs
     - allow domain unsplash

--- project_n/next.config.mjs
+++ project_n/next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ["firebasestorage.googleapis.com"]
-      }
+  images: {
+    domains: ["firebasestorage.googleapis.com", "images.unsplash.com"]
+  }
 };
 
 export default nextConfig;
