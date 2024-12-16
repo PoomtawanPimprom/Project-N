3cfd3663eac9523faea981852ca0c42b4e63f268
Poomtawan Pimprom <poomtawan.work@gmail.com>
2024-12-17 00:42:12 +0700

    create Schema.ts
     - collect Schema for validate

---
+++ project_n/src/lib/zod/Schema.ts
@@ -0,0 +1,28 @@
+import { z, ZodSchema } from "zod";
+
+
+export function validateWithZod<T>(schema: ZodSchema<T>, data: unknown) {
+    const result = schema.safeParse(data);
+    if (!result.success) {
+        const errors = result.error?.errors.map((error) => error.message);
+        throw new Error(errors.join(", "));
+    }
+    return result.data;
+}
+
+export const renderError = (error: unknown): { message: string } => {
+    return { message: error instanceof Error ? error.message : "An Error!!!" };
+  };
+
+// schema
+//========================================================
+export const RegisterSchema = z.object({
+    username: z.string().min(3, "Username ควรมีมากกว่า 3 ตัวอักษร"),
+    email: z.string().email("โปรดกรอก E-mail ให้ถูกต้อง"),
+    password: z.string().min(8, "Password ควรมีมากกว่า 8 ตัวอักษร"),
+});
+
+export const LoginSchema = z.object({
+    username: z.string().min(1,"โปรดกรอก username"),
+    password: z.string().min(1,"โปรดกรอก password"),
+  })
