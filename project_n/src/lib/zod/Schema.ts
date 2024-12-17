4aaef1e5fee9fd8659f412d7f7372c9658ef4ee1
Poomtawan Pimprom <poomtawan.work@gmail.com>
2024-12-17 15:35:33 +0700

    add comment in Schema

--- project_n/src/lib/zod/Schema.ts
+++ project_n/src/lib/zod/Schema.ts
@@ -16,12 +16,15 @@ export const renderError = (error: unknown): { message: string } => {
 
 // schema
 //========================================================
+
+//register
 export const RegisterSchema = z.object({
     username: z.string().min(3, "Username ควรมีมากกว่า 3 ตัวอักษร"),
     email: z.string().email("โปรดกรอก E-mail ให้ถูกต้อง"),
     password: z.string().min(8, "Password ควรมีมากกว่า 8 ตัวอักษร"),
 });
 
+//login
 export const LoginSchema = z.object({
     username: z.string().min(1,"โปรดกรอก username"),
     password: z.string().min(1,"โปรดกรอก password"),
