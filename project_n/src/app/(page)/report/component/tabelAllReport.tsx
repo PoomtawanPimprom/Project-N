2166901394d426a3e474d3b8ad9e00501888ecca
Poomtawan Pimprom <poomtawan.work@gmail.com>
2024-12-18 00:52:23 +0700

    update myreport page
    - make page.tsx to server component
    - create new prop in tableAllReport component to get reportData

--- project_n/src/app/(page)/report/component/tabelAllReport.tsx
+++ project_n/src/app/(page)/report/component/tabelAllReport.tsx
@@ -1,45 +1,16 @@
 "use client";
 import { reportInterface } from "@/app/interface/reportInterface";
-import { getAllReportByUserId } from "@/app/service/report/service";
-import { useEffect, useState } from "react";
+import {  useState } from "react";
 import ModalInfoReport from "./modalInfoReport";
 import TaskStatus from "./TaskStatus";
 
 interface prop {
   userId: number;
+  reportDataByUserID: reportInterface[];
 }
 
-const TabelAllReport = ({ userId }: prop) => {
+const TabelAllReport = ({ userId,reportDataByUserID }: prop) => {
   const [openModal, setOpenModal] = useState(false);
-  const [message, setMessage] = useState("");
-  const [reports, setReports] = useState<reportInterface[]>([]);
-
-  const convertFormatDate = (dataString: string) => {
-    if (dataString === "") {
-      return "Invalid Date";
-    }
-    const date = new Date(dataString);
-
-    const day = date.getDate().toString().padStart(2, "0");
-    const month = (date.getMonth() + 1).toString().padStart(2, "0");
-    const year = date.getFullYear();
-    const hours = date.getHours().toString().padStart(2, "0");
-    const minutes = date.getMinutes().toString().padStart(2, "0");
-    const seconds = date.getSeconds().toString().padStart(2, "0");
-
-    return `${day}-${month}-${year} ${hours}:${minutes}:${seconds}`;
-  };
-
-
-
-  const fetchData = async () => {
-    const data = await getAllReportByUserId(1);
-    setReports(data);
-  };
-
-  useEffect(() => {
-    fetchData();
-  }, []);
   return (
     <div className="flex flex-col w-full h-full px-4 mx-auto ">
       <div className=" mt-4 relative w-full h-full overflow-x-auto border  rounded-lg">
@@ -67,7 +38,7 @@ const TabelAllReport = ({ userId }: prop) => {
             </tr>
           </thead>
           <tbody className="">
-            {reports.map((item, index) => (
+            {reportDataByUserID.map((item, index) => (
               <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
                 <td className="px-6 py-2">{item.comment}</td>
                 <td className="px-6 py-2">
@@ -80,7 +51,6 @@ const TabelAllReport = ({ userId }: prop) => {
                   <button
                     onClick={() => {
                       setOpenModal(true);
-                      setMessage(item.comment);
                     }}
                     className=" rounded-xl text-white px-6 py-2 bg-green-main"
                   >
--- project_n/src/app/(page)/report/myreport/page.tsx
+++ project_n/src/app/(page)/report/myreport/page.tsx
@@ -1,6 +1,27 @@
+import { getServerSession } from "next-auth";
 import TabelAllReport from "../component/tabelAllReport";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { redirect } from "next/navigation";
+import prisma from "@/lib/prisma/db";
+import { reportInterface } from "@/app/interface/reportInterface";
 
-const MyReportPage = () => {
+export default async function MyReportPage() {
+  const session = await getServerSession(authOptions);
+  const userId = Number(session?.user.id);
+  if (!session) {
+    redirect("/login");
+  }
+  const reportDataByUserID = (await prisma.report.findMany({
+    where: { userId: userId },
+    include: {
+      product: true,
+      user: true,
+      reportCategory: true,
+      reportStatus: true,
+    },
+    orderBy: { createdAt: "desc" },
+  })) as reportInterface[];
+
   return (
     <>
       <div className="flex flex-col w-full h-lvh items-center">
@@ -9,12 +30,10 @@ const MyReportPage = () => {
             <p className="ml-4">รายงานของฉัน</p>
           </div>
           <div className=" h-full w-full">
-            <TabelAllReport userId={1} />
+            <TabelAllReport reportDataByUserID={reportDataByUserID} userId={userId} />
           </div>
         </div>
       </div>
     </>
   );
-};
-
-export default MyReportPage;
+}
