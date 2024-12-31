15d1532ab4170ddfa831cf33334dc198ca315e0b
Poomtawan Pimprom <poomtawan.work@gmail.com>
2025-01-01 01:38:45 +0700

    add check session into Product Card Component

--- project_n/src/app/component/productCard.tsx
+++ project_n/src/app/component/productCard.tsx
@@ -1,3 +1,4 @@
+"use client"
 import { FaCartPlus } from "react-icons/fa";
 import LikeButton from "./likeButton";
 
@@ -6,13 +7,36 @@ import { BsThreeDots } from "react-icons/bs";
 import { useRouter } from "next/navigation";
 import { productInterface } from "../interface/productInterface";
 import Image from "next/image";
+import { useSession } from "next-auth/react";
 
 interface prop {
   product: productInterface | any;
 }
 
 const ProductCard = ({ product }: prop) => {
+  const { data:session} = useSession();
   const router = useRouter();
+
+  const handleGotoProductPage = () =>{
+    if(!session){
+      router.push(`/login`)
+      return;
+    }
+    else{
+      router.push(`/product/${product.id}`)
+    }
+  }
+
+  //not complete
+  const handleCart = () =>{
+    if(!session){
+      router.push(`/login`)
+      return;
+    }
+    else{
+      router.push(`/product/${product.id}`)
+    }
+  }
   return (
     <>
       <div className="card flex flex-col w-[275px] border rounded-xl">
@@ -35,12 +59,13 @@ const ProductCard = ({ product }: prop) => {
           <div className="care-price my-1 text-lg text-gray-500">฿{product.price}</div>
           <div className="card-naviButton flex justify-end">
             <button 
+            onClick={handleCart}
             className="flex rounded-full mr-2 border h-10 w-10 justify-center items-center hover:bg-gray-100">
               {/* icon */}
               <FaCartPlus />
             </button>
             <button
-              onClick={() => router.push(`/product/${product.id}`)}
+              onClick={handleGotoProductPage}
               className="flex rounded-full mr-2 border h-10 w-10 justify-center items-center hover:bg-gray-100"
             >
               {/* icon */}
