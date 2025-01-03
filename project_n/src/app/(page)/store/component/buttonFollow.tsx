4ba82bb77f3729b56598161a9f12ccf3b8332b96
Poomtawan Pimprom <poomtawan.work@gmail.com>
2025-01-03 23:20:55 +0700

    update prisma and create Follow button

--- project_n/src/app/(page)/store/component/buttonFollow.tsx
+++ project_n/src/app/(page)/store/component/buttonFollow.tsx
@@ -1,3 +1,12 @@
+"use client";
+import { followInterface } from "@/app/interface/followInterface";
+import {
+  checkFollowed,
+  createFollow,
+  deleteFollowByid,
+} from "@/app/service/follow/service";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 import { CiCirclePlus } from "react-icons/ci";
 
 interface prop {
@@ -5,10 +14,45 @@ interface prop {
   storeId: number;
 }
 
-const ButtonFollow = ({ userId, storeId }: prop) => {
+const ButtonFollow =  ({ userId, storeId }: prop) => {
+  const [isFollowed, setIsFollowed] = useState<followInterface>();
+  const router = useRouter();
+
+  const handleFollowBotton = async () => {
+    //case unAuthenticated
+    if (!userId) {
+      router.replace("/login");
+      return;
+    }
+
+    //case not followed
+    if (isFollowed == null) {
+      const data = {
+        userId,
+        storeId,
+      };
+      await createFollow(data);
+    } else {
+      await deleteFollowByid(isFollowed!.id);
+    }
+  };
+
+  const fetchData = async () => {
+    if (!userId) return null;
+    const isFollowed = await checkFollowed(storeId, userId);
+    console.log(isFollowed);
+    setIsFollowed(isFollowed);
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <>
-      <button className="flex max-h-[60px] bg-white rounded-xl p-3 justify-center items-center text-center hover:bg-gray-100 duration-200">
+      <button
+        onClick={handleFollowBotton}
+        className="flex max-h-[60px] bg-white rounded-xl p-3 justify-center items-center text-center hover:bg-gray-100 duration-200"
+      >
         <CiCirclePlus className="mx-1" />
         Follow
       </button>
