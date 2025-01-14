b209e5ec298467418d62709c22c91c30ade28189
Poomtawan Pimprom <poomtawan.work@gmail.com>
2025-01-15 02:02:40 +0700

    create review page

---
+++ project_n/src/app/(page)/review/page.tsx
@@ -0,0 +1,29 @@
+import prisma from "@/lib/prisma/db";
+import CreateReview from "./CreateReview";
+import { reivewInterface } from "@/app/interface/reviewInterface";
+import ShowComment from "./ShowComment";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { redirect } from "next/navigation";
+
+export default async function ReviewPage() {
+  const session = await getServerSession(authOptions)
+  
+  if (!session) {
+    redirect("/login")
+  }
+  const userId = Number(session.user?.id)
+  const productId = 1;
+  const allReviewsByProductId = (await prisma.review.findMany({
+    where: { productId },
+    include: {
+      user: true,
+    },
+  })) as reivewInterface[];
+  return (
+    <div className="flex flex-col p-4 gap-2">
+      <CreateReview productId={1} userId={userId}/>
+      <ShowComment reviewByProductId={allReviewsByProductId} userId={userId}/>
+    </div>
+  );
+}
