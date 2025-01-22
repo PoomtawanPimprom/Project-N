5682aa16951dd0a16275c9eec96a8c67eeff008e
NamChoco <numchoco2512@gmail.com>
2025-01-22 22:05:23 +0700

    Add api cart by ID

---
+++ project_n/src/app/api/cart/[id]/route.ts
@@ -0,0 +1,54 @@
+import { Prisma, PrismaClient } from '@prisma/client'
+import { NextRequest, NextResponse } from 'next/server';
+
+const prisma = new PrismaClient()
+
+export async function GET(request: NextRequest, { params }: { params: { id: string } }) {
+    try {
+        const userId = Number(params.id);
+        const posts = await prisma.cartItem.findMany({
+            where: { userId: userId },
+            include: { product: true }
+        });
+        return NextResponse.json(posts)
+    } catch (error) {
+        console.error("Error fetching user address:", error);
+        throw new Error("Failed to fetch user address");
+    }
+}
+
+export async function PUT(request: NextRequest, { params }: { params: { id: string } }) {
+    try {
+        const {
+            userId,
+            productId,
+            color,
+            size,
+            quantity,
+        } = await request.json();
+        const cartId = Number(params.id)
+        const updatePost = await prisma.cartItem.update({
+            where: { id: cartId }, data: {
+                userId,
+                productId,
+                color,
+                size,
+                quantity,
+            }
+        });
+        return NextResponse.json(updatePost);
+    } catch (e: any) {
+        return new NextResponse(e instanceof Error ? e.message : String(e), { status: 500 })
+    }
+}
+
+export async function DELETE(request: NextRequest, { params }: { params: { id: string } }) {
+    try {
+        const cartId = Number(params.id);
+        await prisma.cartItem.delete({ where: { id: cartId } });
+        return new NextResponse("Cart delete successfully", { status: 201 });
+    } catch (e: any) {
+        console.log(e);
+        return new NextResponse(e instanceof Error ? e.message : String(e), { status: 500 })
+    }
+}
