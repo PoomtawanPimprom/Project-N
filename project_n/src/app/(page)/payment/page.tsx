cdbc8a608e43edf62db703541d4dfaca398dba4c
Poomtawan Pimprom <poomtawan.work@gmail.com>
2025-01-22 20:55:57 +0700

    update payment page

--- project_n/src/app/(page)/payment/page.tsx
+++ project_n/src/app/(page)/payment/page.tsx
@@ -1,12 +1,14 @@
 import prisma from "@/lib/prisma/db";
-import SelectAddress from "./selectAddress/SelectAddress";
 import { userInterface } from "@/app/interface/userInterface";
-import ProductCart from "./productcart/ProductCart";
-import PaymentType from "./PaymentType/PaymentType";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import { redirect } from "next/navigation";
 import { userAddressInterface } from "@/app/interface/userAddressInterface";
+//component
+import SelectAddress from "./selectAddress/SelectAddress";
+import ProductCart from "./productcart/ProductCart";
+import { productInterface } from "@/app/interface/productInterface";
+import Payment from "./payment/Payment";
 
 export default async function PaymentPage() {
   const session = await getServerSession(authOptions);
@@ -30,18 +32,33 @@ export default async function PaymentPage() {
     where: { userId },
   })) as userAddressInterface[];
 
-  console.log(user);
-  // const paymentType = (await prisma.pa)
+  const productsFromUser =
+    (await prisma.product.findMany()) as productInterface[];
+
+      
+  const amount = 4999;
   return (
     <>
-      <div className="flex flex-col p-4 mx-auto  sm:w-[600px] space-y-2">
-        <SelectAddress
-          user={user}
-          AllUserAddress={AllUserAddress}
-          default_address={userAddressDefault}
-        />
-        <ProductCart />
-        <PaymentType />
+      <div className="grid grid-cols-1 lg:grid-cols-5 p-4 mx-auto max-w-6xl gap-2 ">
+        <div className="col-span-3 ">
+          <div className="flex flex-col gap-2">
+            <SelectAddress
+              user={user}
+              AllUserAddress={AllUserAddress}
+              default_address={userAddressDefault}
+            />
+          <ProductCart products={productsFromUser} />
+          </div>
+
+
+        </div>
+        <div className="col-span-2 ">
+          <div className="flex flex-col gap-2">
+            <div>
+              <Payment amount={500} />
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
