10766148af84d1f39913b2e6ce85332484111f90
Poomtawan Pimprom <poomtawan.work@gmail.com>
2025-01-22 16:17:50 +0700

    Update reviewInterface.ts

--- project_n/src/app/interface/reviewInterface.ts
+++ project_n/src/app/interface/reviewInterface.ts
@@ -6,7 +6,13 @@ export interface reivewInterface {
     id: number;
     comment: string;
     createdAt: Date
-    images?: string[];
+    images?: {
+        image1?: string;
+        image2?: string;
+        image3?: string;
+        image4?: string;
+        image5?: string;
+    };
 
     userId: number;
     user?: userInterface
