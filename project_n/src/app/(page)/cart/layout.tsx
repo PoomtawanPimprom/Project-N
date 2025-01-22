70575874fc320bc3f0b542e266310f9e004a3950
NamChoco <numchoco2512@gmail.com>
2025-01-22 22:07:57 +0700

    Update layout for cart

--- project_n/src/app/(page)/cart/layout.tsx
+++ project_n/src/app/(page)/cart/layout.tsx
@@ -1,18 +1,19 @@
+import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Navbar from "@/app/layout/navbar";
 
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function StoreLayout({
-  children,
+export default function CartLayout({
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <section>
-        <Navbar />
-        {children}
-    </section>
-  );
+    return (
+        <section>
+            <Navbar />
+            {children}
+        </section>
+    );
 }
