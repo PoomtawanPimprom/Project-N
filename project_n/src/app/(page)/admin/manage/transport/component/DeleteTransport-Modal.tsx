3061c9a6971b10e9d5d56263c9b5d93d46bc4f7a
Poomtawan Pimprom <poomtawan.work@gmail.com>
2025-01-24 01:38:46 +0700

    update manage/transport page

--- project_n/src/app/(page)/admin/manage/transport/component/DeleteTransport-Modal.tsx
+++ project_n/src/app/(page)/admin/manage/transport/component/DeleteTransport-Modal.tsx
@@ -2,6 +2,7 @@
 
 import Modal from "@/app/component/modal";
 import { deleteTransportByTransportId } from "@/app/service/admin/transport/service";
+import { AlertTriangle } from "lucide-react";
 import { useRouter } from "next/navigation";
 interface prop {
   open: boolean;
@@ -10,28 +11,32 @@ interface prop {
 }
 
 export default function DeleteTransportModal({ open, onClose, id }: prop) {
-  const router = useRouter()
+  const router = useRouter();
   const DeleteTransport = async () => {
     await deleteTransportByTransportId(id);
-    router.refresh()
+    router.refresh();
   };
   return (
     <>
       <Modal open={open} onClose={onClose}>
         <div className="w-96 gap-2 p-4">
-          <div className="flex text-2xl font-semibold mb-2">
-            คุณต้องการลบบริษัทนี้ออกหรือไม่
+          <div className="flex justify-center mb-2">
+            <AlertTriangle className="w-16 h-16 text-red-500" />
+          </div>
+          <div className="flex w-full text-2xl font-semibold  justify-center  mb-2">
+          <p className="flex">คุณต้องการลบบริษัทนี้ออกหรือไม่</p>
           </div>
           <div className="flex justify-center  gap-2">
-            
-            <button 
-            onClick={onClose}
-            className="rounded-xl bg-gray-500 py-4 text-lg px-6 font-bold text-white transition-colors duration-200 disabled:opacity-50 disabled:cursor-not-allowed">
+            <button
+              onClick={onClose}
+              className="rounded-xl bg-gray-500 py-3 text-lg px-6 font-bold text-white transition-colors duration-200 disabled:opacity-50 disabled:cursor-not-allowed"
+            >
               ยกเลิก
             </button>
             <button
-            onClick={DeleteTransport}
-            className="rounded-xl bg-red-600 py-4 text-lg px-6 font-bold text-white transition-colors duration-200 disabled:opacity-50 disabled:cursor-not-allowed">
+              onClick={DeleteTransport}
+              className="rounded-xl bg-red-600 py-3 text-lg px-6 font-bold text-white transition-colors duration-200 disabled:opacity-50 disabled:cursor-not-allowed"
+            >
               ยืนยัน
             </button>
           </div>
--- project_n/src/app/(page)/admin/manage/transport/page.tsx
+++ project_n/src/app/(page)/admin/manage/transport/page.tsx
@@ -1,24 +1,27 @@
 import { transportInterface } from "@/app/interface/transportInterface";
 import prisma from "@/lib/prisma/db";
 import DataTable from "./component/Table";
-import { Plus } from "lucide-react";
 import CreateTransportButton from "./component/CreateTransport-Button";
+import AdminSideBar from "../../AdminSideBar";
 
 export default async function ManageTransportPage() {
   const allTransports =
     (await prisma.transport.findMany()) as transportInterface[];
 
   return (
-    <div className="mx-auto max-w-7xl space-y-2 px-2">
-      <div className="flex justify-between">
-        <div className="flex">
-          <p className="text-2xl font-semibold mt-8">จัดการบริษัทขนส่ง</p>
+    <div className=" min-h-screen  flex ">
+      <AdminSideBar />
+      <div className="flex-grow p-6 space-y-4 ">
+        <div className="flex justify-between">
+          <div className="flex">
+            <p className="text-3xl font-bold ">จัดการบริษัทขนส่ง</p>
+          </div>
+          <div className="flex items-end">
+            <CreateTransportButton />
+          </div>
         </div>
-        <div className="flex items-end">
-          <CreateTransportButton/>
-        </div>
+        <DataTable transports={allTransports} />
       </div>
-      <DataTable transports={allTransports} />
     </div>
   );
 }
