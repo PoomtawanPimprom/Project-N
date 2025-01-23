9f0a6c574eff80a17ca6026a6e2088a088294bb2
Poomtawan Pimprom <poomtawan.work@gmail.com>
2025-01-24 01:39:41 +0700

    create API for payment system

--- project_n/src/app/api/(payment-system)/checkPayment/route.ts
+++ project_n/src/app/api/(payment-system)/checkPayment/route.ts
@@ -3,20 +3,8 @@ import { NextRequest, NextResponse } from 'next/server';
 
 const prisma = new PrismaClient()
 
-//create
-export async function GET(request: NextRequest) {
-    const { orderId } = await request.json();
-    try {
-        const payment = await prisma.payment.findFirst({
-            where: { orderId: Number(orderId) },
-            select: { paymentStatus: true },
-          });
-        return NextResponse.json({ message: "done" }, { status: 200 });
-    } catch (error: any) {
-        console.error(error.message)
-        return new NextResponse(error instanceof Error ? error.message : String(error), { status: 500 })
-    }
-}
+
+//update want checked
 export async function PUT(request:NextRequest) {
     const {paymentId} = await request.json();
     try {
