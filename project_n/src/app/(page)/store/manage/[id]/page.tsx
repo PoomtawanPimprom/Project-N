fc38eccfaa76178c6ad8b240eda18c4aef40745b
Poomtawan Pimprom <poomtawan.work@gmail.com>
2025-01-25 19:47:24 +0700

    Update page.tsx

--- project_n/src/app/(page)/store/manage/[id]/page.tsx
+++ project_n/src/app/(page)/store/manage/[id]/page.tsx
@@ -15,45 +15,45 @@ export default async function ManageStorePage({
   const products = (await prisma.product.findMany({
     where: { storeID: storeId },
   })) as productInterface[];
-
   const toShipItems = (await prisma.orderItem.findMany({
     where:{storeId:storeId,orderItemStatusId:2}
 })) as orderItemInterface[]
 
-  const statsBoxes = [
-    {
-      icon: <Truck className="w-8 h-8 text-red-500" />,
-      title: "สินค้าที่ต้องส่ง",
-      count: toShipItems.length,
-      bgColor: "bg-red-50",
-      href: `/store/manage/toShip/${storeId}`,
-    },
-    {
-      icon: <Box className="w-8 h-8 text-indigo-500" />,
-      title: "สินค้าทั้งหมด",
-      count: products.length,
-      bgColor: "bg-indigo-50",
-      href: `/store/manage/product/${storeId}`,
-    },
-    {
-      icon: <Warehouse className="w-8 h-8 text-teal-500" />,
-      title: "จัดการคลังสินค้า",
-      bgColor: "bg-teal-50",
-      href: `/store/manage/inventory/${storeId}`,
-    },
-    {
-      icon: <Settings className="w-8 h-8 text-emerald-500" />,
-      title: "แก้ไขรายละเอียดร้านค้า",
-      bgColor: "bg-emerald-50",
-      href: `/store/manage/edit/${storeId}`,
-    },
-    {
-      icon: <Settings className="w-8 h-8 text-amber-500" />,
-      title: "แก้ไขรายละเอียดร้านค้า",
-      bgColor: "bg-amber-50",
-      href: `/store/manage/product/${storeId}`,
-    },
-  ];
+const statsBoxes = [
+  {
+    icon: <Truck className="w-8 h-8 text-red-500" />,
+    title: "สินค้าที่ต้องส่ง",
+    count: toShipItems.length,
+    bgColor: "bg-red-50",
+    href: `/store/manage/toShip/${storeId}`,
+  },
+  {
+    icon: <Box className="w-8 h-8 text-indigo-500" />,
+    title: "สินค้าทั้งหมด",
+    count: products.length,
+    bgColor: "bg-indigo-50",
+    href: `/store/manage/product/${storeId}`,
+  },
+  {
+    icon: <Warehouse className="w-8 h-8 text-teal-500" />,
+    title: "จัดการคลังสินค้า",
+    bgColor: "bg-teal-50",
+    href: `/store/manage/inventory/${storeId}`,
+  },
+  {
+    icon: <Settings className="w-8 h-8 text-emerald-500" />,
+    title: "แก้ไขรายละเอียดร้านค้า",
+    bgColor: "bg-emerald-50",
+    href: `/store/manage/edit/${storeId}`,
+  },
+  {
+    icon: <Settings className="w-8 h-8 text-amber-500" />,
+    title: "แก้ไขรายละเอียดร้านค้า",
+    bgColor: "bg-amber-50",
+    href: `/store/manage/product/${storeId}`,
+  },
+];
+
   return (
     <div className="min-h-screen bg-gray-100 flex">
       <StoreSideBar storeId={storeId.toString()} />
@@ -61,7 +61,7 @@ export default async function ManageStorePage({
         <div className="bg-white h-full  shadow-md rounded-lg p-6 mb-6">
           <div className=" shadow-md rounded-lg p-6 mb-6">
             <h1 className="text-2xl font-semibold mb-4">
-              ยินดีต้อนรับสู่ Admin Dashboard
+              ยินดีต้อนรับสู่ Store Dashboard
             </h1>
           </div>
 
