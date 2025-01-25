3fccf150a23be20788bfc4cf2891239d9c4c19de
Poomtawan Pimprom <poomtawan.work@gmail.com>
2025-01-25 13:17:11 +0700

    update profile/purchase toship, complete page

--- project_n/src/app/(page)/profile/purchase/components/toReceive.tsx
+++ project_n/src/app/(page)/profile/purchase/components/toReceive.tsx
@@ -1,39 +1,84 @@
-import React from 'react'
+"use client";
+
+import { orderItemInterface } from "@/app/interface/orderItemInterface";
+import {
+  GetAllOrderItemsToRecevie,
+  updateStatusOrderItemsToRecevie,
+} from "@/app/service/orderItem/service";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
 export default function ToReceive() {
-    const image = 'https://firebasestorage.googleapis.com/v0/b/project-n-eff9b.firebasestorage.app/o/user-profile.png?alt=media&token=30d9c36c-1638-42d5-82e7-fbd9e6f3e438'
+  const router = useRouter();
+  const { data: session } = useSession();
+  const [OrderItemsToReceive, setOrderItemsToReceive] = useState<
+    orderItemInterface[]
+  >([]);
+  const image =
+    "https://firebasestorage.googleapis.com/v0/b/project-n-eff9b.firebasestorage.app/o/user-profile.png?alt=media&token=30d9c36c-1638-42d5-82e7-fbd9e6f3e438";
 
+  const fecthdata = async () => {
+    const data = await GetAllOrderItemsToRecevie(Number(session?.user.id));
+    setOrderItemsToReceive(data);
+    console.log(data);
+  };
 
+  const onClick = async (id: number) => {
+    await updateStatusOrderItemsToRecevie(id);
+    router.refresh();
+  };
 
-    return (
-        <div className="max-w-7xl mx-auto p-4 hover:shadow-lg rounded-lg">
-            <div className="space-y-4">
-                <div className="flex flex-col sm:flex-row sm:items-center sm:gap-4 p-4 border-b border-gray-200">
-                    {/* Image */}
-                    <div className="w-24 h-24 sm:w-32 sm:h-32">
-                        <img src={image} alt='image' className="w-full h-full object-cover rounded-lg" />
-                    </div>
+  useEffect(() => {
+    fecthdata();
+  }, [session]);
 
-                    {/* Product Info */}
-                    <div className="mt-3 sm:mt-0 flex-1">
-                        <p className="text-lg font-semibold text-gray-800">Name</p>
-                        <p className="text-gray-600">Blue</p>
-                        <p className="text-gray-600">XXL</p>
-                        <p className="text-gray-600">จำนวน: 100x</p>
-                    </div>
-
-                    {/* Price Info */}
-                    <div className="mt-3 sm:mt-0 flex flex-col sm:flex-row items-start sm:items-center gap-2 sm:gap-4">
-                        <p className="text-lg font-semibold text-gray-800">ราคา: 1000</p>
-                        {/* <p className="text-sm text-gray-500 sm:text-lg">ราคารวมทั้งหมด: 200000</p> */}
-                    </div>
-                </div>
+  return (
+    <div className="max-w-7xl mx-auto p-4 hover:shadow-lg rounded-lg">
+      {OrderItemsToReceive.map((orderItem) => (
+        <div className="space-y-4">
+          <div className="flex flex-col sm:flex-row sm:items-center sm:gap-4 p-4 border-b border-gray-200">
+            {/* Image */}
+            <div className="w-24 h-24 sm:w-32 sm:h-32">
+              <img
+                src={orderItem.product?.image?.image1}
+                alt="image"
+                className="w-full h-full object-cover rounded-lg"
+              />
             </div>
 
-            {/* Total Price */}
-            <div className="p-4 border-gray-200 text-right">
-                <p className="text-lg font-semibold text-gray-800">{`ยอดรวมคำสั่งซื้อ: ฿10000000`}</p>
+            {/* Product Info */}
+            <div className="mt-3 sm:mt-0 flex-1">
+              <p className="text-lg font-semibold text-gray-800">
+                {orderItem.product?.name}
+              </p>
+              <p className="text-gray-600">
+                {orderItem.color ? orderItem.color : ""}
+              </p>
+              <p className="text-gray-600">
+                {orderItem.size ? orderItem.size : ""}
+              </p>
+              <p className="text-gray-600">{orderItem.quantity}</p>
             </div>
+
+            {/* Price Info */}
+            <div className="mt-3 sm:mt-0 flex flex-col sm:flex-row items-start sm:items-center gap-2 sm:gap-4">
+              <p className="text-lg font-semibold text-gray-800">
+                ราคา: {orderItem.quantity * orderItem.product?.price!} บาท
+              </p>
+              {/* <p className="text-sm text-gray-500 sm:text-lg">ราคารวมทั้งหมด: 200000</p> */}
+            </div>
+          </div>
+          <div className="flex p-4 border-gray-200 text-right justify-end">
+            <button
+              onClick={() => onClick(orderItem.id)}
+              className="px-6 py-2 bg-green-main text-white rounded-lg "
+            >
+              ได้รับสินค้าแล้ว
+            </button>
+          </div>
         </div>
-    );
+      ))}
+    </div>
+  );
 }
--- project_n/src/app/(page)/profile/purchase/page.tsx
+++ project_n/src/app/(page)/profile/purchase/page.tsx
@@ -6,99 +6,97 @@ import { getUserById } from "@/app/service/profile/service";
 import { userInterface } from "@/app/interface/userInterface";
 import All from "./components/all";
 import ToPay from "./components/toPay";
-import ToShip from "./components/toShip";
 import ToReceive from "./components/toReceive";
 import Complete from "./components/complete";
 import Cancelled from "./components/cancelled";
 import ReturnRefund from "./components/returnRefund";
 
 export default function MyPurchase() {
-    const { data: session } = useSession();
-    const [activeTab, setActiveTab] = useState("ALL");
-    const [userData, setUserData] = useState<userInterface>({
-            id: 0,
-            name: "",
-            username: "",
-            password: "",
-            email: "",
-            mobile: "",
-            birthdate: new Date(),
-            profile: "",
-            saler: false,
-            genderId: 0,
-            roleId: 0,
-            userStatusId: 0,
-            resetToken: "",
-            resetTokenExp: new Date(),
-        });
+  const { data: session } = useSession();
+  const [activeTab, setActiveTab] = useState("ALL");
+  const [userData, setUserData] = useState<userInterface>({
+    id: 0,
+    name: "",
+    username: "",
+    password: "",
+    email: "",
+    mobile: "",
+    birthdate: new Date(),
+    profile: "",
+    saler: false,
+    genderId: 0,
+    roleId: 0,
+    userStatusId: 0,
+    resetToken: "",
+    resetTokenExp: new Date(),
+  });
 
-    const tabs = [
-        { id: "ALL", label: "All" },
-        { id: "TO_PAY", label: "To Pay" },
-        { id: "TO_SHIP", label: "To Ship" },
-        { id: "TO_RECEIVE", label: "To Receive" },
-        { id: "COMPLETE", label: "Complete" },
-        { id: "CANCELLED", label: "Cancelled" },
-        { id: "RETURN_REFUND", label: "Return/Refund" },
-    ];
+  const tabs = [
+    { id: "ALL", label: "All" },
+    { id: "TO_PAY", label: "To Pay" },
+    { id: "TO_SHIP", label: "To Ship" },
+    { id: "TO_RECEIVE", label: "To Receive" },
+    { id: "COMPLETE", label: "Complete" },
+    { id: "CANCELLED", label: "Cancelled" },
+    { id: "RETURN_REFUND", label: "Return/Refund" },
+  ];
 
-    const renderContent = () => {
-        switch (activeTab) {
-            case "ALL":
-                return <All/>;
-            case "TO_PAY":
-                return <ToPay/>;
-            case "TO_SHIP":
-                return <ToShip/>;
-            case "TO_RECEIVE":
-                return <ToReceive/>;
-            case "COMPLETE":
-                return <Complete/>;
-            case "CANCELLED":
-                return <Cancelled/>;
-            case "RETURN_REFUND":
-                return <ReturnRefund/>;
-            default:
-                return <p>Unknown tab selected.</p>;
-        }
-    };
+  const renderContent = () => {
+    switch (activeTab) {
+      case "ALL":
+        return <All />;
+      case "TO_PAY":
+        return <ToPay />;
+      case "TO_RECEIVE":
+        return <ToReceive />;
+      case "COMPLETE":
+        return <Complete />;
+      case "CANCELLED":
+        return <Cancelled />;
+      case "RETURN_REFUND":
+        return <ReturnRefund />;
+      default:
+        return <p>Unknown tab selected.</p>;
+    }
+  };
 
-    const fetchUserData = async () => {
-            const res = await getUserById(Number(session?.user.id));
-            setUserData(res);
-        }
-    
-    
-        useEffect(() => {
-            fetchUserData();
-        }, [session]);
+  const fetchUserData = async () => {
+    const res = await getUserById(Number(session?.user.id));
+    setUserData(res);
+  };
 
-    return (
-        <section id="profile">
-            <div className="container mx-auto flex flex-col lg:flex-row py-6 gap-4 px-4 sm:px-6 lg:px-8">
-                <MenuLeft checkCreatedStore={session?.user.storeId} profile={userData} />
+  useEffect(() => {
+    fetchUserData();
+  }, [session]);
 
-                <div className="flex flex-col lg:w-3/4 gap-4 bg-white border rounded-lg shadow-md p-4 sm:p-6 sm:shadow-none sm:border-black">
-                    <div className="flex border-b">
-                        {tabs.map((tab) => (
-                            <button
-                                key={tab.id}
-                                className={`py-2 px-4 text-sm font-medium ${
-                                    activeTab === tab.id
-                                        ? "border-b-2 border-gray-500 text-gray-500"
-                                        : "text-gray-500"
-                                }`}
-                                onClick={() => setActiveTab(tab.id)}
-                            >
-                                {tab.label}
-                            </button>
-                        ))}
-                    </div>
+  return (
+    <section id="profile">
+      <div className="container mx-auto flex flex-col lg:flex-row py-6 gap-4 px-4 sm:px-6 lg:px-8">
+        <MenuLeft
+          checkCreatedStore={session?.user.storeId}
+          profile={userData}
+        />
 
+        <div className="flex flex-col lg:w-3/4 gap-4 bg-white border rounded-lg shadow-md p-4 sm:p-6 sm:shadow-none sm:border-black">
+          <div className="flex border-b">
+            {tabs.map((tab) => (
+              <button
+                key={tab.id}
+                className={`py-2 px-4 text-sm font-medium ${
+                  activeTab === tab.id
+                    ? "border-b-2 border-gray-500 text-gray-500"
+                    : "text-gray-500"
+                }`}
+                onClick={() => setActiveTab(tab.id)}
+              >
+                {tab.label}
+              </button>
+            ))}
+          </div>
 
-                    <div className="mt-4">{renderContent()}</div>
-                </div>
-            </div>
-        </section>
-    );
+          <div className="mt-4">{renderContent()}</div>
+        </div>
+      </div>
+    </section>
+  );
 }
