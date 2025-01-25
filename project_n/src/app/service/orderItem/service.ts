50d10386eec908e363b94c2cfb5bc710b584cf9a
Poomtawan Pimprom <poomtawan.work@gmail.com>
2025-01-25 13:16:12 +0700

    create orderItem service.ts

---
+++ project_n/src/app/service/orderItem/service.ts
@@ -0,0 +1,31 @@
+const API_URL = '/api/orderItem'
+
+export async function GetAllOrderItemsToRecevie(userId:number) {
+    const res = await fetch(`${API_URL}/toReceive/${userId}`,
+        {
+            method: 'GET',
+            headers: { 'Content-Type': 'application/json' }
+        }
+    )
+    return res.json()
+}
+
+export async function updateStatusOrderItemsToRecevie(userId:number) {
+    const res = await fetch(`${API_URL}/toReceive/${userId}`,
+        {
+            method: 'PUT',
+            headers: { 'Content-Type': 'application/json' }
+        }
+    )
+    return res.json()
+}
+
+export async function GetAllOrderItemsComplete(userId:number) {
+    const res = await fetch(`${API_URL}/complete/${userId}`,
+        {
+            method: 'GET',
+            headers: { 'Content-Type': 'application/json' }
+        }
+    )
+    return res.json()
+}
