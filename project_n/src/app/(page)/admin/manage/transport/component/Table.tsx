af2de5465295f6b8a6ad19ae556977a10f08443d
Poomtawan Pimprom <poomtawan.work@gmail.com>
2025-01-29 23:16:28 +0700

    update admin/manage/transport

--- project_n/src/app/(page)/admin/manage/transport/component/Table.tsx
+++ project_n/src/app/(page)/admin/manage/transport/component/Table.tsx
@@ -4,6 +4,12 @@ import { transportInterface } from "@/app/interface/transportInterface";
 import DeleteTransportModal from "./DeleteTransport-Modal";
 import UpdateTransportModal from "./UpdateTransport-Modal";
 import { SquarePen, Trash2 } from "lucide-react";
+import Table from "@/app/component/table/Table";
+import TableHeader from "@/app/component/table/TableHeader";
+import TableRow from "@/app/component/table/TableRow";
+import TableHead from "@/app/component/table/TableHead";
+import TableBody from "@/app/component/table/TableBody";
+import TableData from "@/app/component/table/Tabledata";
 
 type prop = {
   transports: transportInterface[];
@@ -17,36 +23,36 @@ const DataTable = ({ transports }: prop) => {
       <div className="min-w-full inline-block align-middle">
         <div className="overflow-hidden">
           {/* Desktop view */}
-          <table className="min-w-full hidden md:table">
-            <thead className="bg-gray-50">
-              <tr>
-                <th className="px-6 py-3 text-left text-sm font-semibold text-gray-900">
+          <Table className="w-full text-sm text-left rtl:text-right">
+            <TableHeader className="text-sm 2xl:text-base  text-gray-800  bg-gray-50 dark:bg-black dark:text-accent-foreground">
+              <TableRow className="font-semibold">
+                <TableHead className="px-6 py-3 text-left text-sm  ">
                   ID
-                </th>
-                <th className="px-6 py-3 text-left text-sm font-semibold text-gray-900">
+                </TableHead>
+                <TableHead className="px-6 py-3 text-left text-sm ">
                   Provider Name
-                </th>
-                <th className="px-6 py-3 text-right text-sm font-semibold text-gray-900">
+                </TableHead>
+                <TableHead className="px-6 py-3 text-left text-sm  ">
                   Price
-                </th>
-                <th className="px-6 py-3 text-right text-sm font-semibold text-gray-900">
+                </TableHead>
+                <TableHead className="px-6 py-3 text-right text-sm ">
                   Action
-                </th>
-              </tr>
-            </thead>
-            <tbody className="divide-y divide-gray-200">
+                </TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
               {transports.map((item,index) => (
-                <tr key={index} className="hover:bg-gray-50">
-                  <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                <TableRow key={index} className="bg-white border-b dark:bg-zinc-900  hover:bg-gray-50 dark:hover:bg-zinc-600">
+                  <TableData className="px-6 py-4 ">
                     {item.id}
-                  </td>
-                  <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-600">
+                  </TableData>
+                  <TableData className="px-6 py-4 ">
                     {item.providerName}
-                  </td>
-                  <td className="px-6 py-4 whitespace-nowrap text-sm text-right text-gray-600">
+                  </TableData>
+                  <TableData className="px-6 py-4 ">
                     {item.transportPrice}
-                  </td>
-                  <td className="px-6 py-4 whitespace-nowrap text-right text-sm">
+                  </TableData>
+                  <TableData className="px-6 py-4 whitespace-nowrap text-right text-sm">
                     <div className="flex justify-end gap-2">
                       <button
                         className="flex items-center px-4 py-2 rounded-md text-sm font-medium transition-colors bg-black hover:bg-gray-800 text-white"
@@ -73,11 +79,11 @@ const DataTable = ({ transports }: prop) => {
                         open={openModalDelete}
                       />
                     </div>
-                  </td>
-                </tr>
+                  </TableData>
+                </TableRow>
               ))}
-            </tbody>
-          </table>
+            </TableBody>
+          </Table>
 
           {/* Mobile view */}
           <div className="md:hidden">
