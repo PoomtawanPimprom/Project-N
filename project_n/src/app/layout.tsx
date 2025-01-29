750fedfe8ecd50bc33e447697af7e8f349694499
Poomtawan Pimprom <poomtawan.work@gmail.com>
2025-01-29 12:54:47 +0700

    Update layout.tsx

--- project_n/src/app/layout.tsx
+++ project_n/src/app/layout.tsx
@@ -5,6 +5,8 @@ import { Toaster } from "@/app/components/ui/toaster";
 
 import SessionProvider from "./component/SessionProvider";
 import { getServerSession } from "next-auth";
+import { authOptions } from "./api/auth/[...nextauth]/route";
+import { ThemeProvider } from "./component/ThemeProvider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -20,14 +22,21 @@ export default async function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const session = await getServerSession();
+  const session = await getServerSession(authOptions);
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
         <main className="font-noto">
           <SessionProvider session={session}>
-            {children}
-            </SessionProvider>
+            <ThemeProvider
+              attribute="class"
+              defaultTheme="system"
+              enableSystem
+              disableTransitionOnChange
+            >
+              {children}
+            </ThemeProvider>
+          </SessionProvider>
         </main>
         <Toaster />
       </body>
