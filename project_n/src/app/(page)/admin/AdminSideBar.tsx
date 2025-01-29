bc3bc6deb09a984617e4897c7b8ace9206f29fa9
Poomtawan Pimprom <poomtawan.work@gmail.com>
2025-01-29 23:15:02 +0700

    Update AdminSideBar.tsx

--- project_n/src/app/(page)/admin/AdminSideBar.tsx
+++ project_n/src/app/(page)/admin/AdminSideBar.tsx
@@ -1,5 +1,8 @@
-import { BarChart2, Tag, Truck } from "lucide-react";
+"use client"
+import { BarChart2, Tag, Truck, User } from "lucide-react";
+import { useSession } from "next-auth/react";
 import Link from "next/link";
+
 const menuItems = [
   {
     href: "/admin/manage/report",
@@ -16,25 +19,35 @@ const menuItems = [
     icon: <Truck className="w-6 h-6" />,
     label: "การขนส่ง",
   },
+  {
+    href: "/admin/manage/admins",
+    icon: <User className="w-6 h-6" />,
+    label: "จัดการแอดมิน",
+  },
 ];
-
 export default function AdminSideBar() {
+  const { data: session } = useSession();
   return (
-    <div className="w-64 bg-white shadow-md">
+    <div className="w-64 dark:bg-black dark:border-r  shadow-md">
       <div className="p-5 border-b">
-        <Link href={`/admin`} className="text-xl font-bold text-center">Admin Dashboard</Link>
+        <Link href={`/admin`} className="text-xl font-bold text-center">
+          Admin Dashboard
+        </Link>
       </div>
       <nav className="p-4">
-        {menuItems.map((item) => (
-          <Link
-            key={item.href}
-            href={item.href}
-            className="flex items-center p-3 hover:bg-gray-100 rounded-lg mb-2 transition-colors"
-          >
-            {item.icon}
-            <span className="ml-3 font-medium">{item.label}</span>
-          </Link>
-        ))}
+        {menuItems
+          .filter((item) => session?.user.roleId === "3" || item.label !== "จัดการแอดมิน")
+
+          .map((item) => (
+            <Link
+              key={item.href}
+              href={item.href}
+              className="flex items-center p-3 hover:bg-gray-500 hover:text-white dark:hover:bg-background rounded-lg mb-2 transition-colors"
+            >
+              {item.icon}
+              <span className="ml-3 font-medium">{item.label}</span>
+            </Link>
+          ))}
       </nav>
     </div>
   );
