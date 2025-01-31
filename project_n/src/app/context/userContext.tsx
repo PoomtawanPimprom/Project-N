22f17d7d9cbe6df507cff3c8f99a8b8ea18407f1
Poomtawan Pimprom <poomtawan.work@gmail.com>
2025-01-31 16:35:39 +0700

    create user context and update loot layout

---
+++ project_n/src/app/context/userContext.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import React, { createContext, useContext, useEffect, useState } from "react";
+import { userInterface } from "../interface/userInterface";
+import { useSession } from "next-auth/react";
+import { getUserById } from "../service/profile/service";
+
+interface UserContextType {
+  user: userInterface | null;
+}
+
+const UserContext = createContext<UserContextType>({
+  user: null,
+});
+
+export const UserProvider = ({ children }: { children: React.ReactNode }) => {
+    const {data:session} = useSession();
+    const [user,setUser] = useState<userInterface|null>(null);
+    
+    
+    const fetchUser = async () => {
+        const res = await getUserById(Number(session?.user.id))
+        setUser(res)
+    }
+
+    useEffect(()=>{
+        fetchUser()
+    },[session])
+
+    return(
+        <UserContext.Provider value={{user}}>
+            {children}
+        </UserContext.Provider>
+    )
+};
+
+
+export const useUser = () => useContext(UserContext)
--- project_n/src/app/layout.tsx
+++ project_n/src/app/layout.tsx
@@ -7,6 +7,7 @@ import { getServerSession } from "next-auth";
 import { authOptions } from "./api/auth/[...nextauth]/route";
 import { ThemeProvider } from "./component/ThemeProvider";
 import { CartProvider } from "@/app/context/cartContext";
+import { UserProvider } from "./context/userContext";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -17,17 +18,24 @@ export const metadata: Metadata = {
   },
 };
 
-export default async function RootLayout({ children, }: Readonly<{children: React.ReactNode;}>) {
+export default async function RootLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
   const session = await getServerSession(authOptions);
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
         <main className="font-noto">
           <SessionProvider session={session}>
-            <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange >
-              <CartProvider>
-                {children}
-              </CartProvider>
+            <ThemeProvider
+              attribute="class"
+              defaultTheme="system"
+              enableSystem
+              disableTransitionOnChange
+            >
+              <UserProvider>
+                <CartProvider>{children}</CartProvider>
+              </UserProvider>
             </ThemeProvider>
           </SessionProvider>
         </main>
