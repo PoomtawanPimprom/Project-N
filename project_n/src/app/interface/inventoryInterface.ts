26e98bd6b915d8fb3eea9facbfbe2c4f7e1116ad
Poomtawan Pimprom <poomtawan.work@gmail.com>
2025-03-06 10:13:19 +0700

    Update inventoryInterface.ts

--- project_n/src/app/interface/inventoryInterface.ts
+++ project_n/src/app/interface/inventoryInterface.ts
@@ -1,11 +1,11 @@
 import { productInterface } from "./productInterface"
 
 export interface inventoryInterface {
-    id: number
+    id?: number
     quantity: number |string
     size: string
     color: string
     //fk
-    productID: number
+    productID?: number
     product ?: productInterface
 }
