b30142fb48b9d757da7e06c2f5af3744b4aa7565
Poomtawan Pimprom <poomtawan.work@gmail.com>
2025-04-28 19:24:16 +0700

    update /orderItem/toPay route
    enable toPay Status more than 1

--- project_n/src/app/api/orderItem/toPay/[userId]/route.ts
+++ project_n/src/app/api/orderItem/toPay/[userId]/route.ts
@@ -1,29 +1,42 @@
-import { Prisma, PrismaClient } from '@prisma/client'
-import { NextRequest, NextResponse } from 'next/server';
+import { Prisma, PrismaClient } from "@prisma/client";
+import { NextRequest, NextResponse } from "next/server";
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
-export async function GET(request: NextRequest, props: { params: Promise<{ userId: string }> }) {
-    const params = await props.params;
-    try {
-        const userId = Number(params.userId);
-        //get all order datail status complete
-        const allOrderDetail = await prisma.orderDetail.findMany({
-            where: { userId: userId, orderStatusId: 1 },
-            select: { id: true }
-        });
+export async function GET(
+  request: NextRequest,
+  props: { params: Promise<{ userId: string }> }
+) {
+  const params = await props.params;
+  try {
+    const userId = Number(params.userId);
+    //get all order datail status complete
+    const allOrderDetail = await prisma.orderDetail.findMany({
+      where: { userId: userId, orderStatusId: 1 },
+      select: {
+        createdAt: true,
+        OrderItem: {
+          include: {
+            product: {
+              include: {
+                store: { select: { name: true } },
+              },
+            },
+          },
+        },
+      },
+    });
+    //
+    // const orderDetailIds = allOrderDetail.map(order => order.id);
+    // //get all order item status complete
+    // const allOrderItemsToPay = await prisma.orderItem.findMany({
+    //     where: { orderDetailId: { in: orderDetailIds }, orderItemStatusId: 1 },
+    //     include:{ product: true}
+    // })
 
-        const orderDetailIds = allOrderDetail.map(order => order.id);
-        //get all order item status complete
-        const allOrderItemsToPay = await prisma.orderItem.findMany({
-            where: { orderDetailId: { in: orderDetailIds }, orderItemStatusId: 1 },
-            include:{ product: true}
-        })
-
-
-        return NextResponse.json(allOrderItemsToPay, { status: 200 })
-    } catch (error) {
-        console.error("Error fetching user address:", error);
-        throw new Error("Failed to fetch user address");
-    }
+    return NextResponse.json(allOrderDetail, { status: 200 });
+  } catch (error) {
+    console.error("Error fetching user address:", error);
+    throw new Error("Failed to fetch user address");
+  }
 }
