2f6e8e490cd33b4dfe73d9998ffee3da5593530c
Poomtawan Pimprom <poomtawan.work@gmail.com>
2025-05-03 13:39:10 +0700

    update(waitForShip) groupby product by store and render

--- project_n/src/app/(page)/profile/purchase/components/OrderItemCard.tsx
+++ project_n/src/app/(page)/profile/purchase/components/OrderItemCard.tsx
@@ -1,19 +1,19 @@
 import { orderItemInterface } from "@/app/interface/orderItemInterface";
 
 interface prop {
-  orderItem: orderItemInterface | undefined;
+  product: any;
 }
 
-export default function OrderItemCard({ orderItem }: prop) {
+export default function OrderItemCard({ product }: prop) {
 
  
   const renderWord = (status:number) =>{
     switch (status) {
       case 2:
-        return <p className="text-gray-600">ชำระเงินเมื่อ : {new Date(orderItem.updatedAt).toLocaleDateString()}</p>
+        return <p className="text-gray-600">ชำระเงินเมื่อ : {new Date(product.updatedAt).toLocaleDateString()}</p>
 
       case 3:
-        return<p className="text-gray-600">จัดส่งเมื่อ : {new Date(orderItem.updatedAt).toLocaleDateString()}</p>
+        return<p className="text-gray-600">จัดส่งเมื่อ : {new Date(product.updatedAt).toLocaleDateString()}</p>
     
       default:
         break;
@@ -27,7 +27,7 @@ export default function OrderItemCard({ orderItem }: prop) {
       {/* Image */}
       <div className="w-24 h-24 sm:w-32 sm:h-32">
         <img
-          src={orderItem.product?.image?.image1}
+          src={product.image?.image1}
           alt="image"
           className="w-full h-full object-cover rounded-lg"
         />
@@ -36,16 +36,16 @@ export default function OrderItemCard({ orderItem }: prop) {
       {/* Product Info */}
       <div className="mt-3 sm:mt-0 flex-1">
         <p className="text-lg font-semibold text-gray-800">
-          {orderItem.product?.name}
+          {product.name}
         </p>
         <p className="text-gray-600">
-          {orderItem.color ? "สีของสินค้า : " + orderItem.color : ""}
+          {product.color ? "สีของสินค้า : " + product.color : ""}
         </p>
         <p className="text-gray-600">
-          {orderItem.size ? "ไซส์ของสินค้า : " + orderItem.size : ""}
+          {product.size ? "ไซส์ของสินค้า : " + product.size : ""}
         </p>
-        <p className="text-gray-600">จำนวน : {orderItem.quantity}</p>
-        {renderWord(orderItem.orderItemStatusId)}
+        <p className="text-gray-600">จำนวน : {product.quantity}</p>
+        {renderWord(product.orderItemStatusId)}
         
         
       </div>
@@ -53,7 +53,7 @@ export default function OrderItemCard({ orderItem }: prop) {
       {/* Price Info */}
       <div className="mt-3 sm:mt-0 flex flex-col sm:flex-row items-start sm:items-center gap-2 sm:gap-4">
         <p className="text-lg font-semibold text-gray-800">
-          ราคา: {orderItem.quantity * orderItem.product?.price!} บาท
+          ราคา: {product.quantity * product.price!} บาท
         </p>
       </div>
     </div>
--- project_n/src/app/(page)/profile/purchase/components/waitforShip.tsx
+++ project_n/src/app/(page)/profile/purchase/components/waitforShip.tsx
@@ -12,9 +12,9 @@ export default function WaitforShip() {
   const { toast } = useToast();
   const { data: session } = useSession();
   const [loading, setLoading] = useState(false);
-  const [OrderItemsWaitToShip, setOrderItemsWaitToShip] = useState<orderItemInterface[]>(
-    []
-  );
+  const [OrderItemsWaitToShip, setOrderItemsWaitToShip] = useState<
+    orderItemInterface[]
+  >([]);
 
   // Fetch data from the API
   const fetchData = async () => {
@@ -59,21 +59,78 @@ export default function WaitforShip() {
     }
   }, [session]);
 
+  type groupByStoreType = {
+    name: string;
+    product: any[];
+  };
+
+  const groupedByStore = OrderItemsWaitToShip.reduce<groupByStoreType[]>(
+    (acc, item) => {
+      const storeId = item.product!.storeID;
+      const storeName = item.product!.store!.name;
+
+      const productWithTotal = {
+        ...item.product,
+        orderItemId: item.id,
+        total: item.quantity * item.product!.price,
+        quantity: item.quantity,
+        color: item.color,
+        size: item.size,
+        updatedAt: item.updatedAt,
+        orderItemStatusId: item.orderItemStatusId,
+      };
+    const existingStore = acc.find((store) => store.name === storeName);
+    
+    if (existingStore) {
+      existingStore.product.push(productWithTotal);
+    } else {
+      acc.push({
+        name: storeName,
+        product: [productWithTotal],
+      });
+    }
+    // const existingStore = acc.find((store: any) => {store.name === storeName});
+    // if (existingStore) {
+    //   existingStore.product!.push(productWithTotal);
+    // } else {
+    //   acc.push({
+    //     name: storeName,
+    //     product: [productWithTotal],
+    //   });
+    // }
+
+      return acc;
+    },
+    []
+  );
+
+  console.log(groupedByStore)
+  console.log(OrderItemsWaitToShip)
   return (
     <div className="max-w-7xl mx-auto p-4 hover:shadow-lg rounded-lg">
-      {OrderItemsWaitToShip.map((orderItem, index) => (
-        <div className="flex flex-col space-y-2 border p-4 rounded-lg" key={index}>
-          <OrderItemCard orderItem={orderItem} />
-          <div className="flex justify-end">
-            <button
-              onClick={() => handleButtonClick(orderItem.id)} 
-              disabled={loading}
-              type="button"
-              className={`px-6 py-2 bg-primary text-white rounded-lg ${loading ? "opacity-10": ""}`}
-            >
-              ยังไม่ได้รับสินค้า
-            </button>
-          </div>
+      {groupedByStore.map((store, index) => (
+        <div
+          className="flex flex-col space-y-2 border p-4 rounded-lg"
+          key={index}
+        >
+          <p className="font-bold text-2xl mb-2">{store.name}</p>
+          {store.product.map((item, index) => (
+            <div key={`${item.name} + ${index}`}>
+              <OrderItemCard product={item} />
+              <div className="flex justify-end">
+                <button
+                  onClick={() => handleButtonClick(item.orderItemId)}
+                  disabled={loading}
+                  type="button"
+                  className={`px-6 py-2 bg-primary text-white rounded-lg ${
+                    loading ? "opacity-10" : ""
+                  }`}
+                >
+                  ยังไม่ได้รับสินค้า
+                </button>
+              </div>
+            </div>
+          ))}
         </div>
       ))}
     </div>
