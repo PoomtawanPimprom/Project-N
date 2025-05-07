8669cf8b28bee8436218eb40a73da01e98a1a150
Poomtawan Pimprom <poomtawan.work@gmail.com>
2025-05-07 14:46:41 +0700

    Update navbar.tsx

--- project_n/src/app/component/navbar.tsx
+++ project_n/src/app/component/navbar.tsx
@@ -98,7 +98,7 @@ export default function Navbar() {
             <DropdownMenu>
               <DropdownMenuTrigger asChild>
                 {user !== null ? (
-                  <div className="flex items-center cursor-pointer bg-zinc-50 hover:bg-zinc-200 px-4 py-2 rounded-3xl gap-2">
+                  <div className="flex items-center cursor-pointer bg-zinc-50 hover:bg-zinc-200 px-4 py-2 rounded-3xl gap-2 dark:bg-zinc-700 dark:hover:bg-zinc-600">
                     <img className="dark:border-white w-10 h-10 rounded-full border border-black object-cover" src={user?.profile} alt={user?.name} />
                     <p className="font-bold text-black">{user?.name}</p>
                   </div>
