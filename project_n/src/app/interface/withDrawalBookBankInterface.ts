8a1730f7416db9311cc792294359060f1b9e0c00
Poomtawan Pimprom <poomtawan.work@gmail.com>
2025-05-09 12:49:38 +0700

    Create withDrawalBookBankInterface.ts

---
+++ project_n/src/app/interface/withDrawalBookBankInterface.ts
@@ -0,0 +1,11 @@
+export interface WithDrawalBookBankInterface{
+    id:number
+    accountNumber:string
+    accountName:string
+    bankName:string
+    userId:number
+    default:boolean
+    
+    bookBankTypeId:number
+
+}
