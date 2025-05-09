19ee02bfdff558bb2a63f35686eba4b232cc98e5
Poomtawan Pimprom <poomtawan.work@gmail.com>
2025-05-09 12:49:46 +0700

    Update withdrawalRequestInterface.ts

--- project_n/src/app/interface/withdrawalRequestInterface.ts
+++ project_n/src/app/interface/withdrawalRequestInterface.ts
@@ -1,16 +1,18 @@
 import { storeInterface } from "./storeInterface"
 import { userInterface } from "./userInterface"
+import { WithDrawalBookBankInterface } from "./withDrawalBookBankInterface"
 import { WithDrawalStatusInterface } from "./withDrawalStatusInterface"
 
 export interface WithDrawalRequestInterface{
     id?:number
-    accountNumber :string 
-    accountName   :string 
-    bankName      :string 
+
     amount:number
     createdAt?: Date
     updatedAt?: Date
     message?:string
+
+    bookBankId:number
+    bookBank?:WithDrawalBookBankInterface
 
     statusId:number
     status?:WithDrawalStatusInterface
