ada09251cb7fe320bfae8200f6acc7d7fca4adaa
Poomtawan Pimprom <poomtawan.work@gmail.com>
2025-05-18 20:52:16 +0700

    fix(layout.tsx) update title to ShopKub

--- project_n/src/app/(page)/admin/layout.tsx
+++ project_n/src/app/(page)/admin/layout.tsx
@@ -1,11 +1,6 @@
-import type { Metadata } from "next";
 import Navbar from "@/app/component/navbar";
 
 
-export const metadata: Metadata = {
-  title: "Admin dashboard",
-  description: "Admin dashboard",
-};
 
 export default function StoreLayout({
   children,
--- project_n/src/app/(page)/favorite/layout.tsx
+++ project_n/src/app/(page)/favorite/layout.tsx
@@ -1,11 +1,4 @@
-import type { Metadata } from "next";
 import Navbar from "@/app/component/navbar";
-
-
-export const metadata: Metadata = {
-  title: "favorite",
-  description: "favorite",
-};
 
 export default function StoreLayout({
   children,
